4d020fe60e6ecb082c352f97b566dceacc84e840
Emmanuel Fallancy <emmanuelfallancy@Emmanuels-MacBook-Pro.local>
2016-10-19 15:12:48 +1100

    Make changes to the index file and the js file

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,6 +6,7 @@ cardTwo = "queen";
 cardThree = "king";
 cardFour = "queen";
 
+/*
 if( cardOne == cardThree || cardTwo == cardFour )
 {
 	alert( "You found a match!" );
@@ -13,4 +14,19 @@ if( cardOne == cardThree || cardTwo == cardFour )
 else
 {
 	alert( "Sorry, try again." );
+}
+*/
+
+
+// Assuming there would be 4 cards, this will create the element
+function createCards() {
+	var gameBoard = document.getElementById( "game-board" );
+
+	for( var i = 0; i < 4; i++ )
+	{
+		var card = document.createElement( "div" );
+		//card.setAttribute( 'class', 'card' );
+		card.className = "card";
+		document.getElementById( "game-board" ).appendChild( card );
+	}	
 }
